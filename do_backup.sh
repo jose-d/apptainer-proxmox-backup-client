1cfd20cfc8bf76d579fdd7b038b2dd88c8b34747
jose_d <jose-d@users.noreply.github.com>
2023-09-11 12:46:47 +0200

    instead hardcoded relative sif image path, use variable with absolute path

--- do_backup.sh
+++ do_backup.sh
@@ -2,5 +2,6 @@
 
 export PBS_PASSWORD='FooFoo'
 export PBS_REPOSITORY='user@pbs@pbs1.phoebe.lan:8007:storage'
+PBS_IMAGE='/root/backup/debian-pbs-client.sif'
 
-apptainer exec --bind /:/mnt/sysroot ./debian-pbs-client.sif proxmox-backup-client backup root_disk.pxar:/mnt/sysroot
+apptainer exec --bind /:/mnt/sysroot ${PBS_IMAGE} proxmox-backup-client backup root_disk.pxar:/mnt/sysroot
